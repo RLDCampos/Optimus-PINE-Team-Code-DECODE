472d9994d78b35824693fb32fb6595e3f8e56955
RLDCampos <122762980+RLDCampos@users.noreply.github.com>
2024-12-10 19:17:17 -0500

    New motor power calculation. This code will use calculateCosinePower so that the motor power changes within the cosine mathematical parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAllianceBPlaceAndPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAllianceBPlaceAndPark.java
@@ -33,10 +33,10 @@ public class RedAllianceBPlaceAndPark extends LinearOpMode {
     public void runOpMode() {
         // Initialize motors and servos
         // Declare OpMode members for motors and servos
-        DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
-        leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
+        DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back");
         ySliderMotor = hardwareMap.get(DcMotor.class, "y_slider_motor");
         clawServo = hardwareMap.get(Servo.class, "Claw");
 
