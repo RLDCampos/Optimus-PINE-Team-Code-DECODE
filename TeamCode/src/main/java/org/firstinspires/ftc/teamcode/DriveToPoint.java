8fbba7653c080a3ae11808df22f0503eede3d46c
RLDCampos <122762980+RLDCampos@users.noreply.github.com>
2024-12-18 10:57:31 -0500

    New motor power calculation. This code will use calculateCosinePower so that the motor power changes within the cosine mathematical parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveToPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveToPoint.java
@@ -52,16 +52,16 @@ public class DriveToPoint {
     //Start with 0.001 and increase it if the robot feels sluggish
     // when starting or transitioning between movement
     //Tolerance defines the acceptable margin of error in position for considering the robot "at the target."
-    private static double xyTolerance = 12;// The starting value is 12
+    private static double xyTolerance = 10;// The starting value is 12
     private static double yawTolerance = 0.0349066; // The starting value is 0.0349066
 
     private static double pGain = 0.0080;// The starting value is 0.008
     private static double dGain = 0.00015; // The starting value is 0.00001
     private static double accel = 10.0; // The starting value is 10.0
 
-    private static double yawPGain = 3.0; //The starting value is 5.0
-    private static double yawDGain = 0.1;// The starting value is 0.0
-    private static double yawAccel = 20.0;
+    private static double yawPGain = 5.0; //The starting value is 5.0
+    private static double yawDGain = 0.2;// The starting value is 0.0
+    private static double yawAccel = 15.0;
 
     private double leftFrontMotorOutput  = 0;
     private double rightFrontMotorOutput = 0;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorPinpointDriveToPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorPinpointDriveToPoint.java
@@ -37,10 +37,10 @@ public class SensorPinpointDriveToPoint extends LinearOpMode {
         DRIVE_TO_TARGET_5
     }
 
-    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM,2000,0,AngleUnit.DEGREES,0);
-    static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.MM, 2000, -2000, AngleUnit.DEGREES, -90);
-    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM,200,-2000, AngleUnit.DEGREES,-180);
-    static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.MM, 200,0, AngleUnit.DEGREES, -270);
+    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM,2500,0,AngleUnit.DEGREES,0);
+    static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.MM, 2500, -2500, AngleUnit.DEGREES, -90);
+    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM,200,-2500, AngleUnit.DEGREES,-90);
+    static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.MM, 200,0, AngleUnit.DEGREES, -90);
     static final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM, 200, 0, AngleUnit.DEGREES, 0);
 
 
