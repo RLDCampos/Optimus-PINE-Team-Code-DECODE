2376578730ba5d29302606f865d1fc0358dd141a
RLDCampos <122762980+RLDCampos@users.noreply.github.com>
2025-02-11 19:58:48 -0500

    New motor power calculation. This code will use calculateCosinePower so that the motor power changes within the cosine mathematical parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveToPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveToPoint.java
@@ -55,13 +55,13 @@ public class DriveToPoint {
     private static double xyTolerance = 17;// The starting value is 12
     private static double yawTolerance = 0.0523599; // The starting value is 0.0349066
 
-    private static double pGain = 0.006;// The starting value is 0.008
-    private static double dGain = 0.0001; // The starting value is 0.00001
-    private static double accel = 7.0; // The starting value is 10.0
+    private static double pGain = 0.008;// The starting value is 0.008
+    private static double dGain = 0.00001; // The starting value is 0.00001
+    private static double accel = 10.0; // The starting value is 10.0
 
-    private static double yawPGain = 3.0; //The starting value is 5.0
-    private static double yawDGain = 0.001;// The starting value is 0.0
-    private static double yawAccel = 15.0;
+    private static double yawPGain = 5.0; //The starting value is 5.0
+    private static double yawDGain = 0.018;// The starting value is 0.0
+    private static double yawAccel = 3.0;// It was initially set to 15.0
 
     private double leftFrontMotorOutput  = 0;
     private double rightFrontMotorOutput = 0;
